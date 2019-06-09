03e5f7d8acd09b74f9e7064f87164a0723249eea
summerkim <dasomi6835@gmail.com>
2019-06-10 02:26:24 +0900

    monster 임시업로드
    간략하게 구조를 설명하자면!
    waypoint들은 몬스터경로를 구성하는 포인트이고 play를 누르면 몬스터가 그 포인트들을 따라가염
    그러다 플레이어가 안에들어오면 가장가까운 플레이어 쫒아가고(근데약간버그있네 내일수정할껨)
    그러다 독고기(독버섯그림)가 몬스터 시야(하얀색판)에 들어오면 플레이어 버리고 독고기쫓아가염
    내일 버그랑 애니메이션 고칠께!

---
+++ Assets/Script/Monster_script/Monster_chasing.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster_chasing : MonoBehaviour
+{
+    public Transform[] waypoints;//몬스터의 경로 지점들
+    public float moveSpeed = 2f;//몬스터가 웨이포인트 있는곳들 따라돌때 속도
+    int waypointIndex = 0;
+
+    //public Animator empty_stomach;//공복상태일떄의 애니메이션
+    //int hunger = 100;//몬스터의 허기상태, 0이 되묜 공복상태로 변한다.
+
+    
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        // empty_stomach = GetComponent<Animator>();
+        // Invoke("monster_is_hungry", 10f);
+    }
+
+
+    void Update()
+    {
+        if (!GameObject.FindGameObjectWithTag("monster").GetComponent<vision_of_monster>()
+            .thereismeat)
+            Move();
+        
+
+        /*
+        if (hunger == 0)
+           empty_stomach.SetBool("starvation", true);
+
+        */
+
+    }
+
+    private void Move()
+    {
+        if (waypointIndex < waypoints.Length)
+        {
+            transform.position = Vector3.MoveTowards(transform.position
+            , waypoints[waypointIndex].transform.position, moveSpeed * Time.deltaTime);
+
+
+            if (((int)transform.position.x == (int)waypoints[waypointIndex].transform.position.x)
+                && ((int)transform.position.z == (int)waypoints[waypointIndex].transform.position.z))
+            { waypointIndex += 1; }
+        }
+        else
+        {
+            waypointIndex = 0;
+        }
+    }
+
+    
+
+   /* void monster_is_hungry()//10초마다 몬스터 허기 1씩 깍기게 start에서 invoke함수로 실행
+    {
+        hunger -= 100;
+    }*/
+
+}
---
+++ Assets/Script/Monster_script/vision_of_monster.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vision_of_monster : MonoBehaviour
+{
+    public GameObject monster;
+    public GameObject[] Player = new GameObject[3];
+    public GameObject player_monster_chased;
+
+    Vector3[] distance = new Vector3[3];//플레이어와 몬스터간의 거리벡터
+
+    float[] sqrdistance = new float[3];//거리벡터를 제곱해서 거리를 수치화시킴
+    float Min;//몬스터랑 가장 가까운 플레이어 구하기 위해서
+
+    public GameObject poisened_meat;   //독고기
+    public bool thereismeat = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+    }
+
+    public int count_player_entered = 0;
+
+    private void OnTriggerEnter(Collider coll3)
+    {
+        if (coll3.tag == "Player")
+        {
+            count_player_entered++;
+        }
+
+        if (coll3.tag == "pmeat")
+        {
+            thereismeat = true;
+        }
+    }
+
+
+    private void OnTriggerStay(Collider coll)
+    {
+        if (coll.tag == "pmeat")
+        {
+            GameObject.FindGameObjectWithTag("monster").transform.LookAt(poisened_meat.transform);
+            GameObject.FindGameObjectWithTag("monster").transform.Translate(Vector3.forward * 0.05f);
+        }
+
+
+        else if (coll.tag == "Player")
+        {
+            if (count_player_entered == 1)
+            {
+                monster.transform.LookAt(coll.gameObject.transform);
+                monster.transform.Translate(Vector3.forward * 0.05f);
+            }
+
+            else
+            {
+                for (int i = 0; i < 3; i++)//몬스터와 플레이어사이 거리를 계산하는루프
+                {
+                    distance[i] = monster.transform.position - Player[i].transform.position;
+                    sqrdistance[i] = distance[i].sqrMagnitude;
+                }
+
+                Min = sqrdistance[0];
+
+                for (int i = 0; i < 3; i++)//최소 거리를 Min에 집어넣기
+                {
+                    if (sqrdistance[i] <= Min)
+                    {
+                        Min = sqrdistance[i];
+                        player_monster_chased = Player[i];//최소거리를 갖는 플레이어를 지칭해주기
+                    }
+                }
+                monster.transform.LookAt(player_monster_chased.transform);//지칭된 플레이어를 쫓아가기
+                monster.transform.Translate(Vector3.forward * 0.05f);
+
+
+
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider coll2)
+    {
+        if (coll2.tag == "Player")
+        {
+            count_player_entered--;
+
+        }
+
+        if (coll2.tag == "pmeat")
+        {
+            thereismeat = false;
+        }
+    }
+}
